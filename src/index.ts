7dc3f01ab21366040e10071b2c2fc5c62e578d05
Vipin Meghwal <vipin@dev-007.local>
2023-08-09 10:09:51 +0530

    github actions

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 class Colorify {
-  static success(msg: string) {
-    console.log("\x1b[32m", `[${new Date().toLocaleString()}] [SUCCESS]`, msg);
+  public static success(args: any) {
+    console.log(
+      "\x1b[32m",
+      `[${new Date().toLocaleString()}] [SUCCESS]`,
+      args,
+      "\x1b[0m"
+    );
   }
 
-  static error(msg: any) {
-    console.log("\x1b[31m", `[${new Date().toLocaleString()}] [ERROR]`, msg);
+  public static error(args: any) {
+    console.log(
+      "\x1b[31m",
+      `[${new Date().toLocaleString()}] [ERROR]`,
+      args,
+      "\x1b[0m"
+    );
   }
 
-  static warn(msg: any) {
-    console.log("\x1b[33m", `[${new Date().toLocaleString()}] [WARN]`, msg);
+  public static warn(args: any) {
+    console.log(
+      "\x1b[33m",
+      `[${new Date().toLocaleString()}] [WARN]`,
+      args,
+      "\x1b[0m"
+    );
   }
 
-  static log(msg: any) {
-    console.log(`[${new Date().toLocaleString()}] [LOG]`, msg);
+  public static log(args: any) {
+    console.log(`[${new Date().toLocaleString()}] [LOG]`, args, "\x1b[0m");
   }
 }
 
-const Logger = Colorify;
-export default Logger;
+export const Logger = Colorify;
