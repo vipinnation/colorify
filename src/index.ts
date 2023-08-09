ef48084232bb66aefec81ffe4ca940ca2719eb6c
Vipin Singh Inkiya <62483751+vipinnation@users.noreply.github.com>
2023-08-09 10:01:29 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 class Logger {
-  static success(msg: string) {
+  static success(msg: any) {
     console.log("\x1b[32m", `[${new Date().toLocaleString()}] [SUCCESS]`, msg);
   }
 
-  static error(msg: string) {
+  static error(msg: any) {
     console.log("\x1b[31m", `[${new Date().toLocaleString()}] [ERROR]`, msg);
   }
 
-  static warn(msg: string) {
+  static warn(msg: any) {
     console.log("\x1b[33m", `[${new Date().toLocaleString()}] [WARN]`, msg);
   }
 
-  static log(msg: string) {
+  static log(msg: any) {
     console.log(`[${new Date().toLocaleString()}] [LOG]`, msg);
   }
 }
