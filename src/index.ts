52efce30646cdc347f0c1b3a5e9c082be18ce6a6
Vipin Meghwal <vipinnation@gmail.com>
2024-01-28 11:53:30 +0530

    chores: updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
 class Colorify {
-  public static success(args: any) {
+  public static success(...props: any) {
     console.log(
       "\x1b[32m",
       `[${new Date().toLocaleString()}] [SUCCESS]`,
-      args,
+      ...props,
       "\x1b[0m"
     );
   }
 
-  public static error(args: any) {
+  public static error(...props: any) {
     console.log(
       "\x1b[31m",
       `[${new Date().toLocaleString()}] [ERROR]`,
-      args,
+      ...props,
       "\x1b[0m"
     );
   }
 
-  public static warn(args: any) {
+  public static warn(...props: any) {
     console.log(
       "\x1b[33m",
       `[${new Date().toLocaleString()}] [WARN]`,
-      args,
+      ...props,
       "\x1b[0m"
     );
   }
 
-  public static log(args: any) {
-    console.log(`[${new Date().toLocaleString()}] [LOG]`, args, "\x1b[0m");
+  public static log(...props: any) {
+    console.log(`[${new Date().toLocaleString()}] [LOG]`, ...props, "\x1b[0m");
   }
-}
+} 
 
 export const Logger = Colorify;
